32438cff815908d0b955ef977aba3ebaed3f7ae0
noahingwersen <noah.ingwersen1@gmail.com>
2022-03-27 00:57:46 -0400

    Completed BFS algorithm

--- src/main.py
+++ src/main.py
@@ -1,5 +1,5 @@
-from turtle import circle, pos
-from matplotlib.pyplot import grid
+import threading
+import queue
 import pygame
 import searchers
 from maze import Maze
@@ -30,22 +30,47 @@ class Button:
 class Game:
     START_COLOR = pygame.Color(255, 0, 0)
     GOAL_COLOR = pygame.Color(255, 255, 0)
+    PROGRESS_COLOR = pygame.Color(0, 125, 255)
+    PATH_COLOR = pygame.Color(0, 255, 0)
 
     def __init__(self, maze: Maze, searcher: searchers.Searcher):
         self.maze = maze
         self.searcher = searcher
         self.window = None
+        self.solving = False
     
     def show(self):
         self._initialize()
         self._display()
     
     def reset(self):
+        self.progress = []
+        self.path = []
         self.start_position = None
         self.goal_position = None
+
+        self.progress_queue = None
+        self.path_queue = None
+        self.thread = None
     
     def solve(self):
-        pass
+        if not self.solving:
+            self.show_path = False
+
+            if self.start_position and self.goal_position:
+                self.progress_queue = queue.Queue()
+                self.path_queue = queue.Queue()
+                solve_args = (
+                    self.maze.grid,
+                    self.start_position,
+                    self.goal_position,
+                    self.progress_queue,
+                    self.path_queue
+                )
+
+                self.thread = threading.Thread(target = self.searcher.solve, args=solve_args, daemon=True)
+                self.solving = True
+                self.thread.start()
 
     def _initialize(self):
         maze_width = self.maze.width * 20
@@ -55,6 +80,13 @@ class Game:
 
         self.start_position = None
         self.goal_position = None
+        self.progress_queue = None
+        self.path_queue = None
+        self.thread = None
+        
+        self.progress = []
+        self.path = []
+        self.show_path = False
 
         self.maze_start_x = int((width - maze_width) / 2) if width == 600 else 50 
         self.maze_start_y = 130
@@ -83,6 +115,22 @@ class Game:
         while run:
             self.window.fill(BLACK)
 
+            # Incrementally add search progress with any frame tick so it doesn't overwhelm the user
+            if self.progress_queue:
+                if len(self.progress) != len(self.progress_queue.queue):
+                    next_spot = next(tile for tile in self.progress_queue.queue if tile not in self.progress)
+                    self.progress.append(next_spot)
+                else:
+                    self.show_path = True
+
+            # Add the final path when the thread is complete
+            if self.thread and not self.thread.is_alive():
+                if self.solving and self.path_queue:
+                    for tile in self.path_queue.queue:
+                        self.path.append(tile)
+                    
+                    self.solving = False
+
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     raise SystemExit
@@ -91,7 +139,6 @@ class Game:
                 # depending on if they already exist
                 if event.type == pygame.MOUSEBUTTONDOWN:
                     mouse_position = pygame.mouse.get_pos()
-                    print(mouse_position)
 
                     # Add start/goal positions if inside maze
                     if self.start_position is None:
@@ -103,24 +150,24 @@ class Game:
                     for button in self.buttons:
                         if button.rect.collidepoint(mouse_position):
                             self._button_clicked(button)
-
-
             
-            self._draw_buttons()
-            self._draw_maze()
-            self._draw_message()
-            self._draw_objects()
+            self._draw()
+            self.clock.tick(30)
 
             pygame.display.update()
+    
+    def _draw(self):
+        self._draw_buttons()
+        self._draw_maze()
+        self._draw_message()
+        self._draw_paths()
+        self._draw_objects()
     
     def _create_object(self, position: tuple) -> tuple:
         if self._is_valid(position):
             # Set the objects position in terms of maze array position, not pixel location
             # These will be used by the solver
-            x = int((position[1] - self.maze_start_y) / 20)
-            y = int((position[0] - self.maze_start_x) / 20)
-
-            return (x, y)
+            return self._pixel_to_grid(position)
 
     def _is_valid(self, position: tuple) -> bool:
         # Check if the position is outside maze boundary or a wall
@@ -130,14 +177,21 @@ class Game:
         if position[1] < self.maze_start_y or position[1] > self.maze_end_y:
             return False
         
-        grid_x = int((position[1] - self.maze_start_y) / 20)
-        grid_y = int((position[0] - self.maze_start_x) / 20)
+        grid_x, grid_y = self._pixel_to_grid(position)
 
         if self.maze.grid[grid_x][grid_y] == '#':
             return False
         
         # Position is valid at this point
         return True
+    
+    def _draw_paths(self):
+        for tile in self.progress:
+            self._color_block(tile, self.PROGRESS_COLOR)
+        
+        if self.show_path:
+            for tile in self.path:
+                self._color_block(tile, self.PATH_COLOR)
 
     def _draw_objects(self):
         if self.start_position:
@@ -147,11 +201,8 @@ class Game:
             self._draw_circle(self.goal_position, self.GOAL_COLOR)
 
     def _draw_circle(self, position, color):
-            center = (
-                position[1] * 20 + self.maze_start_x + 10,
-                position[0] * 20 + self.maze_start_y + 10
-            )
-
+            pixel = self._grid_to_pixel(position)
+            center = (pixel[0] + 10, pixel[1] + 10)
             pygame.draw.circle(self.window, color, center, 10)
     
     def _draw_maze(self):
@@ -169,6 +220,11 @@ class Game:
         
         self.maze_end_x = x
         self.maze_end_y = y
+    
+    def _color_block(self, position: tuple, color: pygame.Color):
+        x, y = self._grid_to_pixel(position)
+        rect = pygame.Rect(x, y, 20, 20)
+        pygame.draw.rect(self.window, color, rect)
 
     def _draw_buttons(self):
         for button in self.buttons:
@@ -180,8 +236,27 @@ class Game:
         '''
         if button.name == 'RESET':
             self.reset()
+        
+        if button.name == 'SOLVE':
+            self.solve()
 
-    
+    def _pixel_to_grid(self, pixels):
+        '''
+        Converts pixel location to grid indices
+        '''
+        x = int((pixels[1] - self.maze_start_y) / 20)
+        y = int((pixels[0] - self.maze_start_x) / 20)
+
+        return (x, y)
+
+    def _grid_to_pixel(self, grid):
+        '''
+        Converts grid indices to pixel location
+        '''
+        x = grid[1] * 20 + self.maze_start_x
+        y = grid[0] * 20 + self.maze_start_y
+
+        return (x, y)
 
 if __name__ == '__main__':
     s = searchers.DepthFirstSearcher()
--- src/searchers.py
+++ src/searchers.py
@@ -1,12 +1,87 @@
+import queue
+from typing import Tuple
+from helpers import Stack, Queue, PriorityQueue
+
+class Offset:
+    UP = (-1, 0)
+    DOWN = (1, 0)
+    LEFT = (0, -1)
+    RIGHT = (0, 1)
+
 class Searcher:
-    def solve(self, maze):
+    def solve(self, maze, start, goal):
         pass
 
+    def _is_valid(self, maze, position) -> bool:
+        x, y = position
+        if x < 0 or x > len(maze):
+            return False
+        if y < 0 or y > len(maze[0]):
+            return False
+        
+        if maze[x][y] == '#':
+            return False
+        
+        return True
+
+
 class DepthFirstSearcher(Searcher):
-    pass
+    def __init__(self):
+        self.stack = Stack()
+        self.predecessors = {}
+    
+    def solve(self, maze: list[list[str]], start: Tuple[int, int], goal: Tuple[int, int],
+            progress_queue: queue.Queue = None, path_queue: queue.Queue = None):
+        
+        self.stack.push(start)
+        self.predecessors[start] = None
+
+        while not self.stack.is_empty():
+            position = self.stack.pop()
+            if progress_queue:
+                progress_queue.put(position)
+
+            if position == goal:
+                path = self._get_path(goal)
+
+                # Add path to queue for multithreading
+                if path_queue:
+                    for position in path:
+                        path_queue.put(position)
+                
+                return path
+
+            for direction in [Offset.UP, Offset.RIGHT, Offset.DOWN, Offset.LEFT]:
+                neighbor = (position[0] + direction[0], position[1] + direction[1])
+
+                if self._is_valid(maze, neighbor) and neighbor not in self.predecessors:
+                    self.stack.push(neighbor)
+                    self.predecessors[neighbor] = position
+
+        return None
+
+    def _get_path(self, end) -> list[tuple]:
+        path = []
+        position = end
+        while position is not None:
+            path.append(position)
+            position = self.predecessors[position]
+        
+        path.reverse()
+
+        return path
+
 
 class BredthFirstSearcher(Searcher):
     pass
 
 class AStarSearcher(Searcher):
     pass
+
+
+if __name__ == '__main__':
+    from maze import Maze
+    m = Maze('src/mazes/simple.txt')
+    s = DepthFirstSearcher()
+    test = s.solve(m.grid, (14,1), (2, 10))
+    stop = 'vreak'
